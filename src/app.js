5017b0dd22661b1c3b65f198e48e4e1aef1f7e0a
Anderson Oliveira <andersonpaulojr1@gmail.com>
2025-02-27 11:17:31 -0300

    feat: set up Express server, set up port config and enable ES6 modules
    - Setted up Express server in app.js
    - Updated package.json to support ES6 modules.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from "express"
+import {config} from "../config/index.js"
+const port = config.PORT
+const app = express()
+
+app.listen(port, () => {
+    console.log(`Servidor está rodando em http://localhost:${port}/`)
+})
