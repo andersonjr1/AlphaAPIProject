a5970d5ca06820e0b270b707ba3db4f98bff06cd
Anderson Oliveira <andersonpaulojr1@gmail.com>
2025-03-04 15:57:42 -0300

    feat: add create activity

--- src/pages/create/index.js
+++ src/pages/create/index.js
@@ -2,4 +2,56 @@ import nav from "./nav.js";
 
 const url = "http://localhost:4000/";
 
+const buttonCreateActivity = document.getElementById("buttonCreateActivity");
+
+const inputTitle = document.getElementById("inputTitle");
+
+const inputPlace = document.getElementById("inputPlace");
+
+const inputParticipants = document.getElementById("inputParticipants");
+
+const inputDate = document.getElementById("inputDate");
+
+const textareaDescription = document.getElementById("textareaDescription");
+
+const spanMessage = document.getElementById("spanMessage");
+
+buttonCreateActivity.addEventListener("click", async () => {
+  if (
+    !inputTitle.value ||
+    !inputPlace.value ||
+    !inputParticipants.value ||
+    !inputDate.value
+  ) {
+    spanMessage.innerText = "Preencha todos os valores";
+    return;
+  }
+
+  const response = await fetch(url + "api/activity", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: inputTitle.value,
+      description: textareaDescription.value,
+      data: inputDate.value,
+      place: inputPlace.value,
+      participants: inputParticipants.value,
+    }),
+  });
+  const data = await response.json();
+  console.log(data);
+  if (!response.ok) {
+    spanMessage.innerText = data;
+    return;
+  }
+  inputTitle.value = "";
+  inputPlace.value = "";
+  inputParticipants.value = "";
+  inputDate.value = "";
+  textareaDescription.value = "";
+  spanMessage.innerText = "Atividade criada";
+});
+
 document.querySelector("body").appendChild(nav);
